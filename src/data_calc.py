10c5a97915fd664c43655f29f352406b79761333
Matthew Kenneth Peterson <97690958+MKP157@users.noreply.github.com>
2023-12-03 22:22:09 -0400

    code org, image gen

---
+++ src/data_calc.py
@@ -0,0 +1,44 @@
+import matplotlib.pyplot as plt
+import seaborn as sb
+import pandas as pd
+
+def mattplot() :
+    ###########################################################
+    # Part 2 : Plot each table as regressions                 #
+    ###########################################################
+
+    df = pd.read_parquet('../data/final.parquet')
+
+    plt.figure()
+    sb.regplot(order=2,
+           data=df,
+           y='contributors_per_100k',
+           x='democracy_eiu',
+           color='cornflowerblue')
+    plt.savefig('../pngs/democracy_eiu')
+
+    plt.figure()
+    sb.regplot(order=2,
+           data=df,
+           y='contributors_per_100k',
+           x='pol_part_eiu',
+           color='mediumpurple')
+    plt.savefig('../pngs/pol_part_eiu')
+
+    plt.figure()
+    sb.regplot(order=2,
+           data=df,
+           y='contributors_per_100k',
+           x='dem_culture_eiu',
+           color='palevioletred')
+    plt.savefig('../pngs/dem_culture_eiu',)
+
+    plt.figure()
+    sb.regplot(order=2,
+           data=df,
+           y='contributors_per_100k',
+           x='elect_freefair_eiu',
+           color='coral')
+    plt.savefig('../pngs/elect_freefair_eiu')
+
+    plt.show()
---
+++ src/data_cleanup.py
@@ -0,0 +1,45 @@
+import pandas as pd
+def data_cleanup () :
+    ###########################################################
+    #  Part 1 : Data Cleansing                                #
+    ###########################################################
+    # Import EIU csv, convert to parquet, and filter for required columns
+    # Then query for years as a filter
+    conv('../data/eiu_cleaned.csv',
+         'country_name',
+         'year',
+         'democracy_eiu',
+         'elect_freefair_eiu',
+         'pol_part_eiu',
+         'dem_culture_eiu')
+
+    df_eiu = pd.read_parquet('../data/eiu_cleaned.parquet')
+
+    df_eiu = df_eiu.query("`year` == 2021")
+    print(df_eiu)
+
+    # Import GitHub csv, convert to parquet, and filter for countries and contributors
+    conv('../data/world_countries_2021.csv',
+         'country',
+         'contributors_per_100k')
+
+    df_git = pd.read_parquet('../data/world_countries_2021.parquet')
+    df_git = df_git.query("`country` != 'Montenegro'")
+
+    print(df_git)
+
+    df_joined = pd.merge(left = df_git,
+                         right = df_eiu,
+                         how="right",
+                         left_on='country',
+                         right_on='country_name')
+    print(df_joined)
+    df_joined.to_parquet('../data/final.parquet')
+
+
+# Convert a CSV file to Parquet.
+# First arg is path to CSV, all else are names of columns to include
+def conv( *args ) :
+    df = pd.read_csv(args[0])
+    df = df.filter(items=[x for x in args[1:]])
+    df.to_parquet(args[0].split('.csv')[0] + '.parquet', compression='snappy')
